ec60163b10311e2b275af12b36eff10b63ade7dd
unknown <you@example.com>
2024-09-03 04:44:53 +0200

    contact phase 12

--- assets/js/contacttemplate.js
+++ assets/js/contacttemplate.js
@@ -9,3 +9,9 @@ document.getElementById('cancel-btn').addEventListener('click', function() {
 document.getElementById('close-btn').addEventListener('click', function() {
     document.getElementById('overlay').classList.remove('show');
 });
+
+document.getElementById('overlay').addEventListener('click', function(event) {
+    if (event.target === document.getElementById('overlay')) {
+        document.getElementById('overlay').classList.remove('show');
+    }
+});
