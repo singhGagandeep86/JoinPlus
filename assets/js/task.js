da982b66137e8f4eecbfb90b6db721251e7cb8f3
GagandeepSingh <gagandeep.201119@gmail.com>
2024-09-07 20:56:14 +0200

    javascript cleaned

--- assets/js/task.js
+++ assets/js/task.js
@@ -4,28 +4,36 @@ let base_Url = "https://join-3edee-default-rtdb.europe-west1.firebasedatabase.ap
 
 
 
-function init(){
+function init() {
   fetchUrl();
+  dropdown();
 }
 
-async function fetchUrl(){
-   console.log(`test`);
-   let firebaseUrl = await fetch(base_Url + ".json");
-   let firebaseUrlAsJson = await firebaseUrl.json();
-   console.log(firebaseUrlAsJson);
+async function fetchUrl() {
+  console.log(`test`);
+  let firebaseUrl = await fetch(base_Url + ".json");
+  let firebaseUrlAsJson = await firebaseUrl.json();
+  console.log(firebaseUrlAsJson);
   contactsData(firebaseUrlAsJson);
 }
 
 
 function contactsData(firebase) {
-  let contactsSurname = firebase.contact.nachname;
-  let contactsFirstname = firebase.contact.vorname;
-  console.log(`${contactsFirstname } ` + `${contactsSurname}`);
- let contact = document.getElementById("contacts");
+  let name = firebase.contact.contact.name;
+  let contactsLength = firebase.contact;
+  let objLngth = Object.keys(contactsLength).length;
+  console.log(objLngth);
+  console.log(contactsLength);
+  console.log(name);
+  let contact = document.getElementById("contacts");
+  //for (let i = 2; i < objLngth; i++) {
+  const eachName = contactsLength.contact`${2}`.name;
+  console.log(eachName);
+  // }  
   if (contact.selectedIndex >= 0) {
     const option = document.createElement("option");
-    option.text = `${contactsFirstname} ` + `${contactsSurname}`;
-    const sel = contact.options[contact.selectedIndex + 4];  
+    option.text = `${name} `;
+    const sel = contact.options[contact.selectedIndex + 4];
     contact.add(option, sel);
   }
 }
@@ -72,84 +80,31 @@ function activateLow() {
 
 
 
+function dropdown() {
+  const selectedElement = document.querySelector('.select-selected');
+  const itemsContainer = document.querySelector('.select-items');
+  const items = itemsContainer.querySelectorAll('div');
 
-// this is w3School template
-var x, i, j, l, ll, selElmnt, a, b, c;
-/*look for any elements with the class "assign":*/
-x = document.getElementsByClassName("assign");
-l = x.length;
-for (i = 0; i < l; i++) {
-  selElmnt = x[i].getElementsByTagName("select")[0];
-  ll = selElmnt.length;
-  /*for each element, create a new DIV that will act as the selected item:*/
-  a = document.createElement("DIV");
-  a.setAttribute("class", "select-selected");
-  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-  x[i].appendChild(a);
-  /*for each element, create a new DIV that will contain the option list:*/
-  b = document.createElement("DIV");
-  b.setAttribute("class", "select-items select-hide");
-  for (j = 1; j < ll; j++) {
-    /*for each option in the original select element,
-    create a new DIV that will act as an option item:*/
-    c = document.createElement("DIV");
-    c.innerHTML = selElmnt.options[j].innerHTML;
-    c.addEventListener("click", function (e) {
-      /*when an item is clicked, update the original select box,
-      and the selected item:*/
-      var y, i, k, s, h, sl, yl;
-      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-      sl = s.length;
-      h = this.parentNode.previousSibling;
-      for (i = 0; i < sl; i++) {
-        if (s.options[i].innerHTML == this.innerHTML) {
-          s.selectedIndex = i;
-          h.innerHTML = this.innerHTML;
-          y = this.parentNode.getElementsByClassName("same-as-selected");
-          yl = y.length;
-          for (k = 0; k < yl; k++) {
-            y[k].removeAttribute("class");
-          }
-          this.setAttribute("class", "same-as-selected");
-          break;
-        }
-      }
-      h.click();
-    });
-    b.appendChild(c);
-  }
-  x[i].appendChild(b);
-  a.addEventListener("click", function (e) {
-    /*when the select box is clicked, close any other select boxes,
-    and open/close the current select box:*/
-    e.stopPropagation();
-    closeAllSelect(this);
-    this.nextSibling.classList.toggle("select-hide");
-    this.classList.toggle("select-arrow-active");
+  // Toggle the dropdown when clicking on the selected element
+  selectedElement.addEventListener('click', function () {
+    itemsContainer.classList.toggle('select-hide');
+    this.classList.toggle('select-arrow-active');
   });
-}
-function closeAllSelect(elmnt) {
-  /*a function that will close all select boxes in the document,
-  except the current select box:*/
-  var x, y, i, xl, yl, arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  xl = x.length;
-  yl = y.length;
-  for (i = 0; i < yl; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i)
-    } else {
-      y[i].classList.remove("select-arrow-active");
-    }
-  }
-  for (i = 0; i < xl; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
-    }
-  }
-}
-/*if the user clicks anywhere outside the select box,
-then close all select boxes:*/
-document.addEventListener("click", closeAllSelect);
 
+  // Handle item selection
+  items.forEach(item => {
+    item.addEventListener('click', function () {
+      selectedElement.innerText = this.innerText;
+      selectedElement.setAttribute('data-value', this.getAttribute('data-value'));
+      itemsContainer.classList.add('select-hide');
+      selectedElement.classList.remove('select-arrow-active');
+    });
+  });
+  // Close the dropdown if clicked outside
+  document.addEventListener('click', function (e) {
+    if (!selectedElement.contains(e.target)) {
+      itemsContainer.classList.add('select-hide');
+      selectedElement.classList.remove('select-arrow-active');
+    }
+  });
+};
