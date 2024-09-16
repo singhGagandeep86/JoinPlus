3176375104e5cf7d5595dfe473aa0ee16028e3ae
Rusty-Alex <rustyalex69@googlemail.com>
2024-09-16 17:56:04 +0200

    contact add

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,21 +1,22 @@
 let BASE_URL = "https://join-3edee-default-rtdb.europe-west1.firebasedatabase.app/";
-let path = "";
+let path = '';
 let array = [];
-let draggedElement;
+
 
 async function load() {
-    await loadData("/contacts");
+    await loadData("/contact");
+    loadContact();
 }
 
 async function loadData(path){
     let response = await fetch(BASE_URL + path + ".json");
-    let responsetoJason = await response.json();
+    let responsetoJson = await response.json();
     if (responsetoJson) {
-        let contactsArray = Object.values(responsetoJason);
+        let contactsArray = Object.values(responsetoJson);
         for (let i = 0; i < contactsArray.length; i++) {
             array.push(contactsArray[i]);
         }
-        displayContacts();
+        // displayContacts();
     }
 }
 
@@ -27,4 +28,28 @@ async function postData(path, data) {
         },
         body: JSON.stringify(data)
     });
+}
+
+function loadContact() {
+    let contactSpace = document.getElementById('contactArea');
+    contactSpace.innerHTML = '';
+
+    for (let i = 0; i < array.length; i++) {
+        let contactName = array[i].name;
+        let initials = extrahiereInitialen(contactName)
+        
+       contactSpace.innerHTML +=loadContactData(i, initials);
+    }
+    
+}
+
+function extrahiereInitialen(contactName) {
+    for (let i = 0; i < contactName.length; i++) {
+        let nameParts = contactName.split(' ');
+        let initials = '';
+        for (let j = 0; j < nameParts.length; j++) {
+            initials += nameParts[j].charAt(0).toUpperCase();
+        }
+        return initials;
+    }
 }
