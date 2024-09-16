e70e5faafacb6f3e9f875e91ef1ab723c9b42918
unknown <you@example.com>
2024-09-17 01:59:30 +0200

    master phase 13

--- assets/js/contacttemplate.js
+++ assets/js/contacttemplate.js
@@ -16,29 +16,17 @@ document.getElementById('overlay').addEventListener('click', function(event) {
     }
 });
 
-// function loadContactDataa() {
-//     let loadDoppleData = `
-//     <div class="contact-info">
-//                 <h1>${name}</h1>
-//                 <div class="contact-info-images">
-//                     <img class="editimages" src="/assets/img/edit contacts.png">
-//                     <img class="editimages editimages2" src="/assets/img/Delete contact.png">
-//                 </div>
-//             </div>
-//     `;
-
-//     document.getElementById('contact-container-right').innerHTML = loadDoppleData;
-// }
-
 function loadContactData(i,initials) {
-    return ` <div  class="contact-group">
+    return ` <div class="contact-group">
                 <h2>A</h2>
-                <div class="contact-item ">
+                <div class="contact-item">
                     <div class="avatar"><span class="b-${array[i].color}">${initials}</span></div>
                         <div class="details">
                             <div class="name">${array[i].name}</div>
                             <div class="email">${array[i].email}</div>
                         </div>
                 </div>
-            </div>`
+            </div>`;
 }
+
+
