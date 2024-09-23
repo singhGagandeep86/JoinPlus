de32d56e8da78491efdc76f7dc068c6089afe816
GagandeepSingh <gagandeep.201119@gmail.com>
2024-09-23 23:41:22 +0200

    finish with Layout & cleanCodes

---
+++ assets/js/taskTemplate.js
@@ -0,0 +1,32 @@
+
+
+function contactsTemp(sanitizedEachName, colour, eachName, firstNameStart, lastNameStart) {
+    return `<label>
+    <input type="checkbox"class="checkboxDesign" id="${sanitizedEachName}" onchange="selectionContact('${sanitizedEachName}', '${colour}')">
+    <span value="${sanitizedEachName}"></span>${eachName}
+    <div class="namesInitials b-${colour}">${firstNameStart}${lastNameStart}</div>
+    </label>`;
+}
+
+function subTaskTemp() {
+    return `<div class="smallHead">Subtasks</div>
+    <div class="inputWrapper">
+    <input id="inputField" class="subtasksTxt" placeholder="Add  new subtask" type="text" onfocus="renderSubTask()">
+    <div class="tsksBtns"><img class="inputIcon" src="/assets/img/subTaskCancel.svg" onclick="resetInput()"><img onclick="addList()" class="inputIcon2" src="/assets/img/subTaskEnter.svg"></div>
+</div>`;
+}
+
+function generatedList(subTaskInput) {
+    return `<li onmouseover="hoverEffect(this)" onmouseleave="normalEffect(this)" ondblclick="editsubTask(this)">
+    <div class="leftPart"><span class="bullet"></span>${subTaskInput}</div>
+    <div class="btns subTaskIcon">
+    <img onclick="editsubTask(this, '${subTaskInput}')" class="inputIcon" src="/assets/img/SubtasksEdit.svg">
+    <img onclick="delsubTask(this)" class="deleteIcon" src="/assets/img/SubtasksDel.svg">
+  </div></li>`;
+}
+
+function emptyField() {
+    return `<div class="smallHead">Subtasks</div>
+    <div class="inputWrapper" onclick="renderSubTask()"><input class="subtasksTxt" placeholder="Add new subtask" type="text">
+    <img class="tsksGen" src="/assets/img/subTaskIcon.svg"></div>`;
+}
