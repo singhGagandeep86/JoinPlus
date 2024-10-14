10f392266ece40b7bd6e616075525d1294b7a32b
unknown <you@example.com>
2024-10-14 05:55:18 +0200

    end 2

--- assets/js/submenu.js
+++ assets/js/submenu.js
@@ -1,15 +1,13 @@
-document.getElementById('burgerToggle').addEventListener('click', function() {
-    let menu = document.getElementById('burgerMenu');
-    menu.classList.toggle('open');
+let toggleButton = document.getElementById('burgerToggle');
+let menu = document.getElementById('burgerMenu');
 
+toggleButton.addEventListener('click', function(event) {
+    menu.classList.toggle('open');
     event.stopPropagation();
 });
 
 document.addEventListener('click', function(event) {
-    let menu = document.getElementById('burgerMenu');
-    let toggle = document.getElementById('burgerToggle');
-
-    if (!menu.contains(event.target) && !toggle.contains(event.target)) {
+    if (!menu.contains(event.target) && !toggleButton.contains(event.target)) {
         menu.classList.remove('open');
     }
 });
